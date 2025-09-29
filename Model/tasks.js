9820452b1f4ba9a3a85a9208422cebc61672fb2d
Melkzedk <melkywafula29@gmail.com>
2025-09-29 11:53:54 +0300

    updated task.js

--- Model/tasks.js
+++ Model/tasks.js
@@ -4,7 +4,6 @@ const { Schema } = mongoose;
 
 const taskSchema = new Schema({
   title: { type: String, required: true },
-  description: { type: String },
   status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
   owner: {type: String}
 }, { timestamps: true });
