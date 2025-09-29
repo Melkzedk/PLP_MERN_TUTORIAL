b4b0c6aee9f2f6bd585e7d72a7ff73d0a2accfcf
melkzedek wafula <melkywafula29@gmail.comm>
2025-09-29 11:48:24 +0300

    changes

--- Model/User.js
+++ Model/User.js
@@ -5,5 +5,5 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   name: { type: String, required: true},
   email: { type: String, required: true, unique: true },
-  role: { type: String, default: user },
-});
+  role: { type: String, default: "user"},
+}, { timestamps: true });
