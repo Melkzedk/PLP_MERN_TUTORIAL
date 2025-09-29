36d2260fd9f4e491116f3d321a8f8e9df63467e4
Melkzedk <melkywafula29@gmail.com>
2025-09-29 11:34:29 +0300

    changes

--- db.js
+++ db.js
@@ -1,3 +1,10 @@
 const mongoose = require('mongoose');
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+
+
+//check database connection
+async function connectDB() {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB connected");
+}
