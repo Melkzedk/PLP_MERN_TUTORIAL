6f874398c66cd88c9cd33ce0d7d9849b03a8d85c
Melkzedk <melkywafula29@gmail.com>
2025-09-29 11:43:03 +0300

    changes

---
+++ Model/User.js
@@ -0,0 +1,10 @@
+const { Schema } = require('mongoose');
+const mongoose = require('../db');
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  name: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
