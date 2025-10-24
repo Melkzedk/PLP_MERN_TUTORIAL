9260b3a44d6d83889437b5c03906c3475ede6e7b
Melkzedk <melkywafula29@gmail.com>
2025-10-24 15:41:33 +0300

    changes

---
+++ Model/tasks.js
@@ -0,0 +1,12 @@
+const { mongoose } = require('../db');
+const { Schema } = mongoose;
+
+const taskSchema = new Schema({
+  title: { type: String, required: true },
+  status: { type: String, enum: ['pending', 'in-progress', 'completed'], default: 'pending' },
+  owner: { type: String }
+}, { timestamps: true });
+
+const Task = mongoose.model('Task', taskSchema);
+
+module.exports = Task;
